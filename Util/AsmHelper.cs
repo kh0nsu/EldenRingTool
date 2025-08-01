62bcc683e5602bca08f426865b151c4f98150c6e
Kieran Borg <kierans.borg@gmail.com>
2025-08-01 20:54:16 +0200

    Added force save, infinite poise, rykard no mega, reduced target view

---
+++ Util/AsmHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EldenRingTool.Util
+{
+    public static class AsmHelper
+    {
+        public static int GetRelOffset(IntPtr srcInstrAddr, IntPtr targetAddr, int instrLength = 0)
+            => (int)(targetAddr.ToInt64() - (srcInstrAddr.ToInt64() + instrLength));
+
+        public static byte[] GetRelOffsetBytes(IntPtr srcInstrAddr, IntPtr targetAddr, int instrLength = 0)
+            => BitConverter.GetBytes(GetRelOffset(srcInstrAddr, targetAddr, instrLength));
+
+        public static int GetRelOffset(long srcInstrAddr, long targetAddr, int instrLength = 0)
+            => (int)(targetAddr - (srcInstrAddr + instrLength));
+
+        public static byte[] GetRelOffsetBytes(long srcInstrAddr, long targetAddr, int instrLength = 0)
+            => BitConverter.GetBytes(GetRelOffset(srcInstrAddr, targetAddr, instrLength));
+
+        public static void WriteRelativeOffsets(byte[] bytes,
+            (long baseAddr, long targetAddr, int size, int destinationIndex)[] offsets)
+        {
+            foreach (var (baseAddr, targetAddr, size, destinationIndex) in offsets)
+            {
+                var relativeBytes = GetRelOffsetBytes(baseAddr, targetAddr, size);
+                Array.Copy(relativeBytes, 0, bytes, destinationIndex, 4);
+            }
+        }
+
+        public static byte[] GetJmpOriginOffsetBytes(long hookLocation, int originalInstrLen, IntPtr customCodeEnd)
+            => BitConverter.GetBytes((int)(hookLocation + originalInstrLen - customCodeEnd.ToInt64()));
+
+        public static void WriteJumpOffsets(byte[] bytes,
+            (long hookLocation, int originalInstrLen, IntPtr customCodeAddr, int destinationIndex)[] jumpOffsets)
+        {
+            foreach (var (hookLocation, originalInstrLen, customCodeAddr, destinationIndex) in jumpOffsets)
+            {
+                var originOffsetBytes = GetJmpOriginOffsetBytes(hookLocation, originalInstrLen, customCodeAddr + 5);
+                Array.Copy(originOffsetBytes, 0, bytes, destinationIndex, 4);
+            }
+        }
+        
+        public static byte[] GetAbsAddressBytes(long address)
+            => BitConverter.GetBytes(address);
+        
+        public static void WriteAbsoluteAddresses(byte[] bytes, (long address, int destinationIndex)[] addresses)
+        {
+            foreach (var (address, destinationIndex) in addresses)
+            {
+                var addressBytes = GetAbsAddressBytes(address);
+                Array.Copy(addressBytes, 0, bytes, destinationIndex, 8);
+            }
+        }
+    }
+}
---
+++ Util/HookManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EldenRingTool.Util
+{
+    public class HookManager
+    {
+        private readonly ERProcess _erProcess;
+        private readonly Dictionary<long, HookData> _hookRegistry = new Dictionary<long, HookData>();
+        
+        private class HookData
+        {
+            public long OriginAddr { get; set; }
+            public long CaveAddr { get; set; }
+            public byte[] OriginalBytes { get; set; }
+        }
+        
+        public HookManager(ERProcess erProcess)
+        {
+            _erProcess = erProcess;
+        }
+
+
+        public long InstallHook(long codeLoc, long origin, byte[] originalBytes)
+        {
+            byte[] hookBytes = GetHookBytes(originalBytes.Length, codeLoc, origin);
+            _erProcess.WriteBytes((IntPtr) origin, hookBytes);
+            _hookRegistry[codeLoc] = new HookData
+            {
+                CaveAddr = codeLoc,
+                OriginAddr = origin,
+                OriginalBytes = originalBytes
+            };
+            return codeLoc;
+        }
+
+        private byte[] GetHookBytes(int originalBytesLength, long target, long origin)
+        {
+            byte[] hookBytes = new byte[originalBytesLength];
+            hookBytes[0] = 0xE9;
+
+            int jumpOffset = (int)(target - (origin + 5));
+            byte[] offsetBytes = BitConverter.GetBytes(jumpOffset);
+            Array.Copy(offsetBytes, 0, hookBytes, 1, 4);
+
+            for (int i = 5; i < hookBytes.Length; i++)
+            {
+                hookBytes[i] = 0x90;
+            }
+            return hookBytes;
+        }
+
+        public void UninstallHook(long key)
+        {
+            if (!_hookRegistry.TryGetValue(key, out HookData hookToUninstall))
+            {
+                return;
+            }
+            
+            IntPtr originAddrPtr = (IntPtr)hookToUninstall.OriginAddr;
+            _erProcess.WriteBytes(originAddrPtr, hookToUninstall.OriginalBytes);
+            _hookRegistry.Remove(key);
+
+        }
+
+        public void ClearHooks()
+        {
+            _hookRegistry.Clear();
+        }
+
+        public void UninstallAllHooks()
+        {
+            foreach (var key in _hookRegistry.Keys.ToList())
+            {
+                UninstallHook(key);
+            }
+        }
+    }
+}
